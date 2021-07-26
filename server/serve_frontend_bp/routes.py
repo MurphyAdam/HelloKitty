d983896a7a78c7b0a63be89954886bbeff83116a
Adam Clark <elm.majidi@gmail.com>
2021-07-26 23:47:27 +0100

    removed unused code

--- server/serve_frontend_bp/routes.py
+++ server/serve_frontend_bp/routes.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import send_from_directory
 from server.serve_frontend_bp import serve_frontend_bp
-from flask import current_app
 
 
 @serve_frontend_bp.route('/', defaults={'path': ''})
 @serve_frontend_bp.route('/<path:path>')
 def catch_all(path):
     return send_from_directory('../client/build/', 'index.html')
-
-
-@serve_frontend_bp.route('/uploads/blog/<filename>', methods=['GET'])
-def send_uploaded_file(filename=''):
-    return send_from_directory(current_app.config["UPLOAD_FOLDER"], filename)
