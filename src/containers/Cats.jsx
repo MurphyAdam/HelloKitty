8affb41bdb9cf20573b100c1dcd2d3eced0ee739
Adam Clark <elm.majidi@gmail.com>
2021-07-27 22:57:28 +0100

    added a controlled limit changable by user. + minor UI/UX edit

--- src/containers/Cats.jsx
+++ src/containers/Cats.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect, lazy } from 'react';
-import { Grid, Paper, Typography, makeStyles } from '@material-ui/core';
+import {
+    Grid,
+    Paper,
+    Typography,
+    MenuItem,
+    Select,
+    makeStyles
+} from '@material-ui/core';
 import { fetchCats } from "../services/cat-api";
 
 // we use lazy loading to load out CatView component
@@ -18,21 +25,27 @@ export default function Cats() {
     const classes = useStyles();
     // we hold cats array in the local React state
     const [cats, setCats] = useState([])
-    // isLoading and error are for UI/ UX
+    // isLoading, error and success are for UI/ UX
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false);
+
+    const [limit, setLimit] = useState(25);
+    const limitOptions = [10, 25, 50, 100];
 
     const getCats = () => {
         // let user know we are loading data
         setIsLoading(true);
         // we set error in case it was set to true in the previous load
         setError(false);
-        fetchCats()
+        fetchCats(limit)
             .then(response => {
-                setCats(response.data)
+                setCats(response.data);
+                setSuccess(true);
             })
             .catch(error => {
-                console.log(error)
+                setError('Something went wrong. Please try again later.');
+                console.log(error);
             })
             .finally(() => {
                 setIsLoading(false);
@@ -44,15 +57,23 @@ export default function Cats() {
         // we have not yet performed a request, so we go ahead and do so
         // otherwise we avoid that as we are either waiting for a response 
         // or we got an error (which we inform the user about)
-        if (!cats.length && !isLoading && !error) {
+        if (!success && !error && !isLoading) {
             getCats();
         }
-    }, [cats, error, isLoading])
+        // when the user changes a parameter, we call getCats with the new 
+        // parameter(s) - only if getCats is not already loading.
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [error, isLoading, limit])
+
+    useEffect(() => {
+        getCats(limit);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [limit])
 
     return (
         <div className={classes.root}>
             <Grid container component={Paper} spacing={2}>
-                <Grid item xs={6}>
+                <Grid item xs={4}>
                     <Typography variant="h5"
                         component="h1"
                         className={classes.titleTypo}
@@ -60,10 +81,33 @@ export default function Cats() {
                         Hello, Kitty!
                     </Typography>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={4} align="center">
+                    <Typography variant="h5"
+                        component="span"
+                        className={classes.titleTypo}
+                    >
+                        Limit
+                    </Typography>
+                    <Select
+                        labelId="Limit"
+                        id="limit"
+                        value={limit}
+                        onChange={({ target: { value } }) => setLimit(value)}
+                        fullWidth
+                    >
+                        {limitOptions.map(limit => {
+                            return (
+                                <MenuItem key={limit} value={limit} disabled={isLoading}>
+                                    {limit}
+                                </MenuItem>
+                            )
+                        })}
+                    </Select>
+                </Grid>
+                <Grid item xs={4}>
                     <Typography variant="h5"
                         align="right"
-                        component="h1"
+                        component="h2"
                         className={classes.titleTypo}
                     >
                         A meow API
@@ -78,7 +122,7 @@ export default function Cats() {
                                 align="center"
                                 className={classes.titleTypo}
                             >
-                                Loading cats...
+                                Loading a limit of {limit} cats...
                             </Typography>
                         </Grid>
                     )
